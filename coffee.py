1a6ab425dbd2d202a312b2aa607c2a5231511534
Teemu Kokkonen <teemu@fastmonkeys.com>
2016-09-09 15:25:23 +0300

    General fixes

--- coffee.py
+++ coffee.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 import time
 
 import requests
@@ -7,18 +8,23 @@ from flowdock import TeamInbox
 
 FLOWDOCK_API_KEY = os.environ.get('FLOWDOCK_API_KEY')
 COFFEE_URL = os.environ.get('COFFEE_URL', 'http://192.168.179.80:8080/')
-SIMULATION = bool(os.environ.get('SIMULATION', True))
+USE_SAMPLE_DATA = bool(os.environ.get('USE_SAMPLE_DATA', True))
 
 def normalize(x):
     return min(1, float(x) / 65)
 
-if SIMULATION:
+if not FLOWDOCK_API_KEY:
+    print('FLOWDOCK_API_KEY not set, using simulated team box...')
     class DummyTeamInbox(object):
         def post(self, *args, **kwargs):
             print('POST:', args, kwargs)
     inbox = DummyTeamInbox()
 else:
     inbox = TeamInbox(FLOWDOCK_API_KEY)
+
+SAMPLE_ROWS = open('sample_data.txt').readlines()
+if USE_SAMPLE_DATA:
+    print('Using sample data...')
 
 COFFEE_NO = 0
 COFFEE_COMING = 1
@@ -29,8 +35,10 @@ coffee_level = 0.0
 i = 4
 while True:
     i += 1
-    if SIMULATION:
-        coffee_levels = list(map(normalize, [int(x.strip()) for x in open('sample_data.txt').readlines()]))[0:i]
+    if USE_SAMPLE_DATA:
+        coffee_levels = list(map(normalize, [int(x.strip()) for x in SAMPLE_ROWS]))[0:i]
+        if i >= len(SAMPLE_ROWS):
+            sys.exit(0)
     else:
         req = requests.get(COFFEE_URL)
         coffee_levels = [
@@ -52,7 +60,7 @@ while True:
             'Barista',
             'teemu+barista@fastmonkeys.com',
             'Coffee is ready!',
-            '<p>%s</p>' % (':coffee:' * cups_of_coffee)
+            '<p>%s</p>' % (':coffee: ' * cups_of_coffee)
         )  # , tags='@team'
     elif incoming_coffee == COFFEE_READY and now <= 0.1:
         incoming_coffee = COFFEE_NO
@@ -74,10 +82,10 @@ while True:
             'Barista',
             'teemu+barista@fastmonkeys.com',
             'Coffee is consumed',
-            '<p>%s</p>' % (':coffee:' * cups_of_coffee)
+            '<p>%s</p>' % (':coffee: ' * cups_of_coffee)
         )
 
-    if SIMULATION:
-        time.sleep(0.1)
+    if USE_SAMPLE_DATA:
+        time.sleep(0.05)
     else:
         time.sleep(2)
