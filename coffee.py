be010c1c4618abcaeff6c2e2df5f2f014f32a4aa
Teemu Kokkonen <teemu@fastmonkeys.com>
2016-09-09 14:58:33 +0300

    PEP8ify and fix Python 3 issues

--- coffee.py
+++ coffee.py
@@ -33,7 +33,9 @@ while True:
         coffee_levels = list(map(normalize, [int(x.strip()) for x in open('sample_data.txt').readlines()]))[0:i]
     else:
         req = requests.get(COFFEE_URL)
-        coffee_levels = map(normalize, map(int, re.findall('([0-9]+)<', req.content)))
+        coffee_levels = [
+            normalize(int(x)) for x in re.findall('([0-9]+)<', req.content.decode('utf-8'))
+        ]
 
     now = coffee_levels[-1]
     diff1 = coffee_levels[-1] - coffee_levels[-2]
@@ -46,14 +48,34 @@ while True:
         coffee_level = now
         incoming_coffee = COFFEE_READY
         cups_of_coffee = int(round(now * 10))
-        inbox.post('Barista', 'teemu+barista@fastmonkeys.com', 'Coffee is ready!', '<p>%s</p>' % (':coffee:' * cups_of_coffee))  # , tags='@team'
+        inbox.post(
+            'Barista',
+            'teemu+barista@fastmonkeys.com',
+            'Coffee is ready!',
+            '<p>%s</p>' % (':coffee:' * cups_of_coffee)
+        )  # , tags='@team'
     elif incoming_coffee == COFFEE_READY and now <= 0.1:
         incoming_coffee = COFFEE_NO
-        inbox.post('Barista', 'teemu+barista@fastmonkeys.com', 'Out of coffee', '<p></p>')
-    elif incoming_coffee == COFFEE_READY and abs(diff1) < 0.05 and diff2 < -0.05 and (coffee_level-now) > 0.05:
+        inbox.post(
+            'Barista',
+            'teemu+barista@fastmonkeys.com',
+            'Out of coffee',
+            '<p></p>'
+        )
+    elif (
+        incoming_coffee == COFFEE_READY and
+        abs(diff1) < 0.05 and
+        diff2 < -0.05 and
+        (coffee_level-now) > 0.05
+    ):
         coffee_level = now
         cups_of_coffee = int(round(now * 10))
-        inbox.post('Barista', 'teemu+barista@fastmonkeys.com', 'Coffee is consumed', '<p>%s</p>' % (':coffee:' * cups_of_coffee))
+        inbox.post(
+            'Barista',
+            'teemu+barista@fastmonkeys.com',
+            'Coffee is consumed',
+            '<p>%s</p>' % (':coffee:' * cups_of_coffee)
+        )
 
     if SIMULATION:
         time.sleep(0.1)
